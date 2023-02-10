8aba7146957e4bd4bdd7de6f952192fa0678c7d4
sharol2021 <sharollopez@colegioguanenta.edu.co>
2023-02-10 10:13:05 -0500

    se agregro el diagrama,proyecto y README

---
+++ nota_definitiva_asignatura.py
@@ -0,0 +1,15 @@
+# programa para calcular la nota definitiva de una asignatura en la especialidad de sistemas
+
+# input
+np = int(input("digite el valor de np: "))
+nc = int(input("digite el valor de nc: "))
+na = int(input("digite el valor de na: "))
+au = int(input("digite el valor de au: "))
+bi = int(input("digite el valor de bi: "))
+
+# processing 
+nd = (0.3*np) + (0.3*nc) + (0.1*na) + (0.1*au) + (0.2*bi)
+
+#output
+
+print("la nota definitiva de " + str(nd))
